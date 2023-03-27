9cd01a8c3c9945948fdd2d9f47adeabb3fdb9723
tatiana <tgz57031@gmail.com>
2023-03-27 11:48:17 -0500

    supuestas mejoras

--- calendario/jscalendario.js
+++ calendario/jscalendario.js
@@ -1,7 +1,27 @@
-//getDate trae el dia de 1 a 31 
 //getDay de 1 a 6
-
+//getDate trae el dia de 1 a 31 
+let meses = ["Enero","Febrero","Marzo","Abril",
+            "Mayo","Junio","Julio","Agosto","Septiembre",
+            "Octubre","Noviembre","Diciembre"]
 let date = new Date();
-let numDia = date.getDay();
-let active = document.querySelector(".semana li:nth-child("+numDia+")");
+let nomDia = date.getDay();
+
+let diaMes = date.getDate();
+let mes = meses[date.getMonth()];
+let año = date.getFullYear()
+
+let active = document.querySelector(".semana li:nth-child("+nomDia+")");
 active.classList.add(`actual`);
+
+let h1 = document.createElement("h1");
+h1.innerHTML = diaMes;
+active.appendChild(h1)
+
+let h2 = document.createElement("h2");
+h2.innerHTML = mes + " del " + año;
+active.appendChild(h2)
+//------------
+const elementoLi = document.querySelector('.semana li.actual');
+const estiloLi = getComputedStyle(elementoLi, '::after');
+const contenidoAfter = estiloLi.getPropertyValue('n');
+
